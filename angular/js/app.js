8b9f3e894e997d648145bdf82fc37231ac5bce17
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-20 13:01:53 -0500

    Create and use controllers, and directives like: ng-class, ng-click, ng-src; function calls in index, etc...

--- angular/js/app.js
+++ angular/js/app.js
@@ -48,9 +48,33 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('StoreController', function(){
+  app.controller('StoreController', function() {
     this.products = gems;
   });
+
+  app.controller('PanelController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+  // app.controller('GalleryController', function() {
+  //   this.current = 0;
+    
+  //   this.setCurrent = function(newCurrent) {
+  //     if (newCurrent === null) {
+  //       this.current = 0;
+  //     } else {
+  //       this.current = newCurrent;
+  //     }
+  //   };
+  // });
 
   var gems = [{
       name: 'Azurite',
